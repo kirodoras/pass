0e3413194dc8e70ba964c472bae04d452a448898
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-08 00:35:33 -0300

    feat: set users repository

---
+++ src/repositories/users.repository.ts
@@ -0,0 +1,16 @@
+import client from "../database";
+import * as usersTypes from "../types/users.types";
+
+export async function insert(user: usersTypes.User) {
+  const result = await client.users.create({
+    data: user,
+  });
+  return result;
+}
+
+export async function select(email: string) {
+  const result = await client.users.findUnique({
+    where: { email },
+  });
+  return result;
+}
