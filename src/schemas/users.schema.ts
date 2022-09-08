b539fc7b93fbc6046106c6ce48a6e4b8976913cd
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-07 23:59:18 -0300

    refactor: add type in users schema

--- src/schemas/users.schema.ts
+++ src/schemas/users.schema.ts
@@ -1,6 +1,7 @@
 import joi from "joi";
+import * as usersTypes from "../types/users.types";
 
-const usersSchema = joi.object({
+const usersSchema = joi.object<usersTypes.User>({
   email: joi.string().email().required(),
   password: joi.string().min(10).required(),
 });
