fb4fb5e22deaa982ddd8fb15095c1d7b96e8816f
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-08 14:07:07 -0300

    feat: set signIn in users service

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,6 +1,7 @@
 import * as usersTypes from "../types/users.types";
 import * as usersRepository from "../repositories/users.repository";
 import * as bcryptProvider from "../providers/bcrypt.provider";
+import * as jwtProvider from "../providers/jwt.provider";
 
 export async function signUp(user: usersTypes.User) {
   const { email, password } = user;
@@ -15,4 +16,23 @@ export async function checkUserExists(email: string) {
   if (user) {
     throw { type: "conflict", message: "User already exists" };
   }
+}
+
+export async function signIn(user: usersTypes.User) {
+  const { email, password } = user;
+  const userMatch = await checkUserNotExists(email);
+  const match = bcryptProvider.verify(password, userMatch.password);
+  if (!match) {
+    throw { type: "unauthorized", message: "Invalid request" };
+  }
+  const token = jwtProvider.encode({ email });
+  return token;
+}
+
+export async function checkUserNotExists(email: string) {
+  const user = await usersRepository.select(email);
+  if (!user) {
+    throw { type: "unauthorized", message: "Invalid request" };
+  }
+  return user;
 }
