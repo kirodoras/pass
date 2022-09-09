7f69c5bdb1b6717a1ce665e8bd953345b1a844e3
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-09 15:12:52 -0300

    feat: set find all in notes repository

--- src/repositories/notes.repository.ts
+++ src/repositories/notes.repository.ts
@@ -28,6 +28,15 @@ export async function findById(id: number, user_id: number) {
   return result;
 }
 
+export async function findAll(user_id: number) {
+  const result = await client.notes.findMany({
+    where: {
+      user_id,
+    },
+  });
+  return result;
+}
+
 export async function deleteById(id: number) {
   const result = await client.notes.delete({
     where: {
