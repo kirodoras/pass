d1a509953918a7053639d192a23b92f09ba385bd
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-09 14:18:46 -0300

    feat: set find by id in wifis controller

--- src/services/wifis.services.ts
+++ src/services/wifis.services.ts
@@ -7,3 +7,14 @@ export async function create(wifi: wifisTypes.Wifi) {
   wifi.network_password = hash_password;
   await wifisRepository.insert(wifi);
 }
+
+export async function findById(id: number, user_id: number) {
+  const wifi = await wifisRepository.findById(id, user_id);
+  if (!wifi) {
+    throw { type: "not_found", message: "Wifi not found" };
+  }
+  const { network_password } = wifi;
+  const decrypted_password = cryptrProvider.decrypt(network_password);
+  wifi.network_password = decrypted_password;
+  return wifi;
+}
