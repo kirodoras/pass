9814276f6c92efa416cc73a4417bc228629e0d44
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-10 20:32:33 -0300

    feat: set finds in cards repository

--- src/repositories/cards.repository.ts
+++ src/repositories/cards.repository.ts
@@ -17,3 +17,22 @@ export async function findByTittleAndUserId(tittle: string, user_id: number) {
   });
   return result;
 }
+
+export async function findById(id: number, user_id: number) {
+  const result = await client.cards.findFirst({
+    where: {
+      id,
+      user_id,
+    },
+  });
+  return result;
+}
+
+export async function findAll(user_id: number) {
+  const result = await client.cards.findMany({
+    where: {
+      user_id,
+    },
+  });
+  return result;
+}
