ac9c4ee85ee883b728dd42a70ab44c1015f8e49f
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-15 18:16:03 -0300

    configure jest e supertest setup

---
+++ tests/app.test.ts
@@ -0,0 +1,23 @@
+import app from "../src/app";
+import supertest from "supertest";
+
+console.log(process.env.DATABASE_URL);
+
+beforeEach(() => {
+  // essa função será executada antes de cada it() rodar
+  //await client.$executeRaw`TRUNCATE TABLE [TABLE-NAME];`;
+});
+
+describe("GET /", () => {
+  it("should return 200, Online", async () => {
+    const result = await supertest(app).get("/");
+    const status = result.status;
+
+    expect(status).toEqual(200);
+  });
+});
+
+afterAll(() => {
+  // essa função será executada ao final de todos os testes
+  //await client.$disconnect();
+});
